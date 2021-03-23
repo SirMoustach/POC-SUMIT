948adc0e14124a1a0f4ecc91c045a19add7068e9
Natacha Rome <natacha-rome@hotmail.fr>
2021-03-23 14:58:32 +0100

    First commit

---
+++ POC-SUMIT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace POC_SUMIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
